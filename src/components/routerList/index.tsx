7743608a10f5652b6474472ba29d37c1176a48f3
ianSama0217 <ian20000217@gmail>
2025-07-07 17:07:47 +0800

    routerList style

--- src/components/routerList/index.tsx
+++ src/components/routerList/index.tsx
@@ -5,11 +5,21 @@ import { Routes, Route, Link } from "react-router-dom";
 const RouterList: React.FC = () => {
   return (
     <div>
-      {routerListData.map((item: any, index: number) => (
-        <Link key={index} to={item.path}>
-          {item.title}
-        </Link>
-      ))}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          borderBottom: "1px solid black",
+        }}
+      >
+        {routerListData.map((item: any, index: number) => (
+          <Link key={index} to={item.path}>
+            {item.title}
+          </Link>
+        ))}
+      </div>
+
       <Routes>
         {routerListData.map((item: any, index: number) => (
           <Route key={index} path={item.path} element={item.element} />
